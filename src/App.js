2e37f846ce53fc1c169e01f07cb7cc5efaf9bfe6
patrickford <patrickford@msn.com>
2021-02-11 23:40:58 -0800

    launched

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
+import VideoPlayer from './VideoPlayer.js';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App"> 
       <header className="App-header">
-        <img src='./img/comediaplex-banner.jpg' className="App-logo" alt="logo" />
+        <img src='./img/comediaplex-banner.jpg' className="App-logo" height="400" alt="logo" />
       </header>
+      <VideoPlayer/>
     </div>
   );
 }
