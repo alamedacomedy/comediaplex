cd65a14bf045f099e36158ce527ca0dbe4bd0cdd
patrickford <patrickford@msn.com>
2021-02-15 11:08:08 -0800

    updated banner

--- src/VideoPlayer.js
+++ src/VideoPlayer.js
@@ -1,15 +1,20 @@
 function VideoPlayer() {
 
-
     return (
         <div>
-            <h1 class="red">Join us for our Valentine's Weekend Special starring Monique Marvez</h1>
+            {/* <h1 class="red">Join us for our Valentine's Weekend Special starring Monique Marvez</h1>
             <h2>Live from the Alameda Comedy Club</h2>
             <a href="https://play.webvideocore.net/popapp.php?l=508536&w=720&h=800&p=4GE35AGFE12D4C4&title=Monique+Marvez+-+Live+from+the+Alameda+Comedy+Club+-+Friday+2%2F13+at+6pm+Pacific&bgcolor1=%23ffffff&bgcolor2=%23e0e0e0&hide_playlist=&hide_description=&hide_live_chat=1&layout=default&is_inversed=&theme=light&image=https%3A%2F%2Fmember.streamingvideoprovider.com%2Fpanel%2Fserver%2Fclip%3Fa%3DGenerateThumbnail%26clip_id%3D3508959%26size%3Dlarge%26is_recorded%3D3508959&use_html5=1&live_id=&sel_playlist=214bf27i50b6e84&sel_multiplaylist=&is_responsive=1&is_vertical=&one_thumb_per_row=1&thumbs_size=medium&disable_hash=1&skinAlpha=50&colorBase=%23250864&colorIcon=&colorHighlight=%237f54f8&fs_popin=&start_volume=&close_button=&player_align=NONE&player_bar=1&auto_play=&auto_hide_player_controls=1&chat_position=&description_position=&playlist_position=&allow_fullscreen=1&player_start_volume=&widget_height_behavior=0&template_published_fields=-1&play_button=1&play_button_style=pulsing&show_auto_play_next=1&auto_play_next=1&floating_player=none" rel="noreferrer" target="_blank">
                 Monique Marvez Live - Fri-Sat Feb 12th and 13th at 6:00pm and 8:00pm Pacific</a><br></br><br></br>
             <a href="https://play.webvideocore.net/popapp.php?l=508536&w=720&h=800&p=4GE35AGFE12D4C4&title=Sun+2%2F14+Noon+-+Drag+Yourself+to+Brunch&bgcolor1=%23ffffff&bgcolor2=%23e0e0e0&hide_playlist=&hide_description=&hide_live_chat=1&layout=default&is_inversed=&theme=light&image=https%3A%2F%2Fmember.streamingvideoprovider.com%2Fpanel%2Fserver%2Fclip%3Fa%3DGenerateThumbnail%26clip_id%3D3515480%26size%3Dlarge%26is_recorded%3D3515480&use_html5=1&live_id=&sel_playlist=214bf27i50b6e84&sel_multiplaylist=&is_responsive=1&is_vertical=&one_thumb_per_row=1&thumbs_size=medium&disable_hash=1&skinAlpha=50&colorBase=%23250864&colorIcon=&colorHighlight=%237f54f8&fs_popin=&start_volume=&close_button=&player_align=NONE&player_bar=1&auto_play=&auto_hide_player_controls=1&chat_position=&description_position=&playlist_position=&allow_fullscreen=1&player_start_volume=&widget_height_behavior=0&template_published_fields=-1&play_button=1&play_button_style=pulsing&show_auto_play_next=1&auto_play_next=1&floating_player=none" rel="noreferrer" target="_blank">
-                Drag Yourself to Brunch - Sun Feb 14th at noon Pacific</a><br></br><br></br>
-            <h3>More shows will be listed shortly</h3>
+                Drag Yourself to Brunch - Sun Feb 14th at noon Pacific</a><br></br><br></br> */}
+            <h1>Upcoming shows:</h1>
+
+                <h3>Feb 19-20 Mario Hodge with Jack Assadorian Jr.</h3>
+                <h3>Feb 26-27 Kirk McHenry with Chris Suji</h3>
+                <h3>Mar 5 - 6 Taylor Tomlinson and Irene Tu</h3>
+                <h3>Mar 12-13 Jen Murphy with Doug Ferrari</h3>
+
         </div>
     )
 }
