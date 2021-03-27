54dc778d14d4ed17905cef0aa861001e2c74e316
patrickford <patrickford@msn.com>
2021-03-26 18:31:25 -0700

    errors

--- src/VideoPlayer.js
+++ src/VideoPlayer.js
@@ -2,16 +2,18 @@ function VideoPlayer() {
 
     return (
         <div>
-            <h2>Live from the Alameda Comedy Club</h2>
-            <a href="https://play.webvideocore.net/popapp.php?l=551256&w=720&h=800&p=3B28EE1H9G9JA8G&title=Erica+Rhodes+-+Fri+6pm+Pacific&bgcolor1=%23ffffff&bgcolor2=%23e0e0e0&hide_playlist=&hide_description=1&hide_live_chat=1&layout=default&is_inversed=&theme=light&image=https%3A%2F%2Fmember.streamingvideoprovider.com%2Fpanel%2Fserver%2Fclip%3Fa%3DGenerateThumbnail%26clip_id%3D3660547%26size%3Dlarge%26is_recorded%3D3660547&use_html5=1&live_id=&sel_playlist=44id1gjjdgah040&sel_multiplaylist=&is_responsive=1&is_vertical=&one_thumb_per_row=1&thumbs_size=medium&disable_hash=1&skinAlpha=50&colorBase=%23250864&colorIcon=&colorHighlight=%237f54f8&fs_popin=&start_volume=&close_button=&player_align=NONE&player_bar=1&auto_play=&auto_hide_player_controls=1&chat_position=&description_position=&playlist_position=&allow_fullscreen=1&player_start_volume=&widget_height_behavior=0&template_published_fields=-1&play_button=1&play_button_style=pulsing&show_auto_play_next=1&auto_play_next=1&floating_player=none" rel="noreferrer" target="_blank">
-                <h3>Erica Rhodes Live - Fri-Sat March 19th and 20th at 6:00pm and 8:00pm Pacific</h3>
-                <img src="./img/EricaRhodesPromo.jpg" height="300" alt="Erica Rhodes"></img>
+            <h1>We are having technical issues with the stream and are working on it</h1>
+            <h2>We will issue refunds for any customers affected</h2>
+            <a href="https://play.webvideocore.net/popapp.php?l=557292&w=720&h=800&p=4GE35AGFE12D4C4&title=Hal+Sparks+-+6pm+Friday+-+Live+from+the+Alameda+Comedy+Club&bgcolor1=%23ffffff&bgcolor2=%23e0e0e0&hide_playlist=&hide_description=&hide_live_chat=1&layout=default&is_inversed=&theme=light&image=https%3A%2F%2Fmember.streamingvideoprovider.com%2Fpanel%2Fserver%2Fclip%3Fa%3DGenerateThumbnail%26clip_id%3D3682459%26size%3Dlarge%26is_recorded%3D3682459&use_html5=1&live_id=&sel_playlist=3jj0796fg21a80c&sel_multiplaylist=&is_responsive=1&is_vertical=&one_thumb_per_row=1&thumbs_size=medium&disable_hash=1&skinAlpha=50&colorBase=%23250864&colorIcon=&colorHighlight=%237f54f8&fs_popin=&start_volume=&close_button=&player_align=NONE&player_bar=1&auto_play=&auto_hide_player_controls=1&chat_position=&description_position=&playlist_position=&allow_fullscreen=1&player_start_volume=&widget_height_behavior=0&template_published_fields=-1&play_button=1&play_button_style=pulsing&show_auto_play_next=1&auto_play_next=1&floating_player=none" rel="noreferrer" target="_blank">
+                <h3>Hal Sparks Live - Fri-Sat March 26th and 27th at 6:00pm and 8:00pm Pacific</h3>
+                <img src="./img/HalSparksPromo.jpg" height="300" alt="Hal Sparks"></img>
             </a><br></br><br></br>
 
             <h1>Upcoming shows:</h1>
-            <h3>Mar 26-27 Hal Sparks</h3>
             <h3>Apr 2-3 Eddie Pepitone</h3>
-
+            <h3>Apr 9-10 Adam Cozens</h3>
+            <h3>Apr 16-18 Clinton Jackson with Sandy Stec</h3>
+            <h3>Apr 23-24 Cathy Ladman</h3>
         </div>
     )
 }
