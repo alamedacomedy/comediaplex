7525ec5354d17286861ebce974bebb576118cad2
patrickford <patrickford@msn.com>
2021-04-21 12:46:38 -0700

    Cathy Ladman

--- src/VideoPlayer.js
+++ src/VideoPlayer.js
@@ -2,18 +2,14 @@ function VideoPlayer() {
 
     return (
         <div>
-            <h1>We are having technical issues with the stream and are working on it</h1>
-            <h2>We will issue refunds for any customers affected</h2>
-            <a href="https://play.webvideocore.net/popapp.php?l=557292&w=720&h=800&p=4GE35AGFE12D4C4&title=Hal+Sparks+-+6pm+Friday+-+Live+from+the+Alameda+Comedy+Club&bgcolor1=%23ffffff&bgcolor2=%23e0e0e0&hide_playlist=&hide_description=&hide_live_chat=1&layout=default&is_inversed=&theme=light&image=https%3A%2F%2Fmember.streamingvideoprovider.com%2Fpanel%2Fserver%2Fclip%3Fa%3DGenerateThumbnail%26clip_id%3D3682459%26size%3Dlarge%26is_recorded%3D3682459&use_html5=1&live_id=&sel_playlist=3jj0796fg21a80c&sel_multiplaylist=&is_responsive=1&is_vertical=&one_thumb_per_row=1&thumbs_size=medium&disable_hash=1&skinAlpha=50&colorBase=%23250864&colorIcon=&colorHighlight=%237f54f8&fs_popin=&start_volume=&close_button=&player_align=NONE&player_bar=1&auto_play=&auto_hide_player_controls=1&chat_position=&description_position=&playlist_position=&allow_fullscreen=1&player_start_volume=&widget_height_behavior=0&template_published_fields=-1&play_button=1&play_button_style=pulsing&show_auto_play_next=1&auto_play_next=1&floating_player=none" rel="noreferrer" target="_blank">
-                <h3>Hal Sparks Live - Fri-Sat March 26th and 27th at 6:00pm and 8:00pm Pacific</h3>
-                <img src="./img/HalSparksPromo.jpg" height="300" alt="Hal Sparks"></img>
-            </a><br></br><br></br>
+ 
+            <a href='https://play.webvideocore.net/popapp.php?l=588172&w=720&h=800&p=4GE35AGFE12D4C4&title=Cathy+Ladman+-+Friday+7pm+Pacific&bgcolor1=%23ffffff&bgcolor2=%23e0e0e0&hide_playlist=&hide_description=&hide_live_chat=1&layout=default&is_inversed=&theme=light&image=https%3A%2F%2Fmember.streamingvideoprovider.com%2Fpanel%2Fserver%2Fclip%3Fa%3DGenerateThumbnail%26clip_id%3D3806225%26size%3Dlarge%26is_recorded%3D3806225&use_html5=1&live_id=&sel_playlist=52i36e01f296c4c&sel_multiplaylist=&is_responsive=1&is_vertical=&one_thumb_per_row=1&thumbs_size=medium&disable_hash=1&skinAlpha=50&colorBase=%23250864&colorIcon=&colorHighlight=%237f54f8&fs_popin=&start_volume=&close_button=&player_align=NONE&player_bar=1&auto_play=&auto_hide_player_controls=1&chat_position=&description_position=&playlist_position=&allow_fullscreen=1&player_start_volume=&widget_height_behavior=0&template_published_fields=-1&play_button=1&play_button_style=pulsing&show_auto_play_next=1&auto_play_next=1&floating_player=none' rel="noopener noreferrer" target="_blank">
+                <h2>Cathy Ladman Live on Pay Per View</h2>
+                <h3>Fri-Sat April 23rd and 24th at 7:00pm and 9:00pm Pacific</h3>
+                <img src="./img/CathyLadmanPromo.jpg" height="300" alt="Cathy Ladman"></img>
+            </a><br></br><br></br> 
+<h1>We will be announcing more shows soon.</h1>
 
-            <h1>Upcoming shows:</h1>
-            <h3>Apr 2-3 Eddie Pepitone</h3>
-            <h3>Apr 9-10 Adam Cozens</h3>
-            <h3>Apr 16-18 Clinton Jackson with Sandy Stec</h3>
-            <h3>Apr 23-24 Cathy Ladman</h3>
         </div>
     )
 }
